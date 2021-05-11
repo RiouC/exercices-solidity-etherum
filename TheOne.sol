823a76f3c6f9a73ed3ac98a9df24fb15a22123df
RiouC <riou.christophe@protonmail.com>
2021-05-11 15:08:57 +0200

    [TheOne] add TheOne.sol

---
+++ TheOne.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity  ^0.8.0;
+
+/**
+ * @title TheOne
+ * @dev Implements a function one returning 1
+ */
+contract TheOne {
+    function one() public pure returns (uint256) {
+        return 1;
+    }
+}
